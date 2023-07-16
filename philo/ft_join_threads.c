7ce6aebd1415f8c6237107d335f6471c9a118a83
tmoumni <tmoumni@student.42.fr>
2023-07-16 14:09:23 +0100

    mutex for 1 philo

--- philo/ft_join_threads.c
+++ philo/ft_join_threads.c
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 17:54:55 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/07/15 15:51:51 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/07/16 14:08:21 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ void	ft_join_threads(t_args *args, t_philo *philos)
 	int	i;
 
 	i = 0;
-	if (args->philos_num == 1)
-	{
-		pthread_mutex_unlock(&(args->forks[philos[0].left]));
-	}
 	while (i < args->philos_num)
 	{
 		if (pthread_join(philos[i].thread, NULL))
